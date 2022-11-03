bcf306e481a1c4346f84cea46c6cba903d65462e
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-11-03 09:40:03 +0530

    Update Array.c

--- Array.c
+++ Array.c
@@ -1,138 +1,314 @@
+// header files
 #include <stdio.h>
 #include <stdlib.h>
-struct Array
+
+// global variables
+int menuOption, process = 0;
+
+// array and its properties
+struct array
 {
-    int *A;
-    int size;
-    int length;
-};
-void Display(struct Array arr)
+    int counter;
+    int array1[10];
+} a1;
+
+// setting value of variables inside the struct
+void setValues()
 {
-    int i;
-    printf("\nElements are\n");
-    for (i = 0; i < arr.length; i++)
-        printf("%d ", arr.A[i]);
+    a1.counter = 0;
 }
-void Append(struct Array *arr, int x)
+
+// main menu
+int menu()
 {
-    if (arr->length < arr->size)
-        arr->A[arr->length++] = x;
+    printf("Press 1 for insertion\n");
+    printf("Press 2 for deletion\n");
+    printf("Press 3 for traversing\n");
+    printf("Press 4 for linear search\n");
+    printf("Press 5 for selection sort\n");
+    printf("Press 9 to exit\n");
+    scanf("%d", &menuOption);
 }
-void Insert(struct Array *arr, int index, int x)
+
+int menu1()
 {
-    int i;
-    if (index >= 0 && index <= arr->length)
+    printf("Press 1 for insertion\n");
+    printf("Press 9 to exit\n");
+    scanf("%d", &menuOption);
+}
+
+// operation functions
+void printArr()
+{
+    if (a1.counter == 0)
     {
-        for (i = arr->length; i > index; i--)
-            arr->A[i] = arr->A[i - 1];
-        arr->A[index] = x;
-        arr->length++;
+        printf("The array is empty\n");
     }
-}
-int Delete(struct Array *arr, int index)
-{
-    int x = 0;
-    int i;
-    if (index >= 0 && index < arr->length)
+    else
     {
-        x = arr->A[index];
-        for (i = index; i < arr->length - 1; i++)
-            arr->A[i] = arr->A[i + 1];
-        arr->length--;
-        return x;
-    }
-    return 0;
-}
-void swap(int *x, int *y)
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-int LinearSearch(struct Array *arr, int key)
-{
-    int i;
-    for (i = 0; i < arr->length; i++)
-    {
-        if (key == arr->A[i])
+        printf("The array is : ");
+        for (int i = 0; i < a1.counter - 1; i++)
         {
-            return i;
+            printf("%d, ", a1.array1[i]);
         }
+        printf("%d\n", a1.array1[a1.counter - 1]);
     }
-    return -1;
+    printf("\n");
 }
 
-void selectionSort(struct Array *arr)
+int insertion()
 {
-    for (int i = 0; i < arr->length - 1; i++)
+    int element, index, choice;
+    int beginning()
     {
-        int minIndex = i;
-
-        for (int j = i + 1; j < arr->length; j++)
+        printf("Enter element to insert at the beginnning\n");
+        scanf("%d", &element);
+        if (a1.counter>0)
         {
-            if (arr->A[j] < arr->A[minIndex])
+            for (int i = a1.counter; i >=0 ; i--)
             {
-                   minIndex = j;
+                a1.array1[i] = a1.array1[i-1];
             }
         }
+        a1.array1[0] = element;
+        a1.counter++;
+        printArr();
+    }
+    int atIndex()
+    {
+        printf("Enter the index: ");
+        scanf("%d", &index);
+        if (index<=a1.counter)
+        {    
+            printf("Enter element to insert at the index:%d\n", index);
+            scanf("%d", &element);
+            
+            for (int i = a1.counter; i > index; i--)
+            {
+                a1.array1[i] = a1.array1[i-1];
+            }
+            a1.array1[index] = element;
+            a1.counter++;
+            printArr();
+        }
+        // checks
+        else
+        {
+            printf("Please enter correct index");
+        }
+        
+    }
+    int end()
+    {
+        printf("Enter element to insert at the end\n");
+        scanf("%d", &element);
+        a1.array1[a1.counter] = element;
+        a1.counter++;
+        printArr();
+    }
+insertOneCheckpoint:
+    switch (a1.counter)
+    {
+    case 0:
+        beginning();
+        break;
 
-        swap(arr->A[i], arr->A[minIndex]);
+    case 2 ... 9:
+        printf("Enter 1 to enter in the beginning\n");
+        printf("Enter 2 to enter in the end\n");
+        printf("Enter 3 to enter at a specific index\n");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            beginning();
+        }
+        else if (choice == 2)
+        {
+            end();
+        }
+        else if (choice == 3)
+        {
+            atIndex();
+        }
+        else
+        {
+            printf("Choice out of range\n");
+            goto insertOneCheckpoint;
+        }
+        break;
+    default:
+        printf("Index out of bounds\n");
+        break;
+
+    case 1:
+        printf("Enter 1 to enter in the beginning\n");
+        printf("Enter 2 to enter in the end\n");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            beginning();
+        }
+        else if (choice == 2)
+        {
+            end();
+        }
+        else
+        {
+            printf("Choice out of range\n");
+            goto insertOneCheckpoint;
+        }
+        break;
     }
 }
 
+int deletion()
+{
+    int index;
+insertDeleteCheckpoint:
+    printf("Enter the index of the element you want to delete\n");
+    scanf("%d", &index);
+    if (index >= 0 && index <= a1.counter)
+    {
+        for (int i = index; i <= a1.counter; i++)
+        {
+            a1.array1[i] = a1.array1[i + 1];
+        }
+        a1.counter--;
+        printArr();
+    }
+    else
+    {
+        printf("Index out of range\n");
+        goto insertDeleteCheckpoint;
+    }
+}
 
+int traversing()
+{
+    int index;
+insertTraverseCheckpoint:
+    printf("Enter the index of the element\n");
+    scanf("%d", &index);
+    if (index >= 0 && index <= a1.counter)
+    {
+        printf("%d", a1.array1[index]);
+    }
+    else
+    {
+        printf("Index out of range");
+        goto insertTraverseCheckpoint;
+    }
+    printArr();
+}
+
+int traversing2(){
+    for (int i = 0; i < a1.counter; i++)
+    {
+        printf("%d\n",i);
+    }
+    printArr();
+}
+
+int searching()
+{
+    int element, crash = -1;
+insertSearchingCheckpoint:
+    printf("Enter the element you want to search for: \n");
+    scanf("%d", &element);
+    for (int i = 0; i < a1.counter; i++)
+    {
+        if (a1.array1[i] == element)
+        {
+            crash = i;
+        }
+    }
+    if (crash >= 0)
+    {
+        printf("The index of %d is %d\n", element, crash);
+    }
+    else
+    {
+        printf("No element found\n");
+        goto insertSearchingCheckpoint;
+    }
+}
+
+int sorting()
+{
+    int min, crash, temp;
+    for (int i = 0; i < a1.counter; i++)
+    {
+        min = a1.array1[i];
+        for (int j = i + 1; j < a1.counter; j++)
+        {
+            if (a1.array1[j] < min)
+            {
+                min = a1.array1[j];
+                crash = j;
+            }
+        }
+        temp = a1.array1[i];
+        a1.array1[i] = min;
+        a1.array1[crash] = temp;
+    }
+    printArr();
+}
+
+// main function
 int main()
 {
-    struct Array arr1;
-    int ch;
-    int x, index;
-    printf("Enter Size of Array");
-    scanf("%d", &arr1.size);
-    arr1.A = (int *)malloc(arr1.size * sizeof(int));
-    arr1.length = 0;
-    do
+    setValues();
+    printArr();
+showMenu:
+    if (a1.counter == 0)
     {
-        printf("\n\nMenu\n");
-        
-        printf("1. Insert\n");
-        printf("2. Delete\n");
-        printf("3. Search\n");
-        printf("4. Display\n");
-        printf("5. Sort\n");
-        printf("6.Exit\n");
-        printf("enter you choice ");
-        scanf("%d", &ch);
-        switch (ch)
+        menu1();
+        switch (menuOption)
         {
         case 1:
-            printf("Enter an element and index");
-            scanf("%d%d", &x, &index);
-            Insert(&arr1, index, x);
+            insertion();
             break;
-        case 2:
-            printf("Enter index ");
-            scanf("%d", &index);
-            x = Delete(&arr1, index);
-            printf("Deleted Element is %d\n", x);
+        case 9:
+            exit(0);
             break;
-        case 3:
-            printf("Enter element to search ");
-            scanf("%d", &x);
-            index = LinearSearch(&arr1, x);
-            printf("Element index %d", index);
-            break;
-        case 4:
-            Display(arr1);
-            break;
-         case 5:
-            if (arr1.length > 1)
-            {
-                selectionSort(&arr1);
-            }
-            Display(arr1);
+        default:
+            printf("Please enter correct input\n");
+            goto showMenu;
             break;
         }
-    } while (ch < 6);
+    }
+
+    else
+    {
+        menu();
+        switch (menuOption)
+        {
+        case 1:
+            insertion();
+            break;
+        case 2:
+            deletion();
+            break;
+        case 3:
+            traversing2();
+            break;
+        case 4:
+            searching();
+            break;
+        case 5:
+            sorting();
+            break;
+        case 9:
+            exit(0);
+            break;
+        default:
+            printf("Please enter correct input\n");
+            goto showMenu;
+            break;
+        }
+    }
+
+    // making sure our menu runs inside a loop
+    goto showMenu;
     return 0;
 }
